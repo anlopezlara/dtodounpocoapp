bee2c4ec5c91e1e49787a93228c12a5bbf30b254
ANDRES Lopez <anlopezlara@gmail.com>
2018-01-31 11:08:24 -0800

    primer boton

--- examples/extrae_productos.php
+++ examples/extrae_productos.php
@@ -9,13 +9,13 @@ $result = $meli->get($url, $params);
 echo '<pre>';
 print_r('<p>4</p>');
 
-print_r($result["body"]["paging"]);
-/*foreach($result["body"] as $nivel => $value) { 
+print_r($result["body"]);
+foreach($result["body"] as $nivel => $value) { 
 	print_r($value);
 	print_r('<p>####################VAVLUE######################################</p>');
-    print_r($nivel);
+	print_r($nivel);
 	print_r('<p>####################NIVEL######################################</p>');
-} */
+}
 print_r('<p>************************************************************</p>');
 #foreach ($result as $i => $value) {
 #    unset($array[$i]);
