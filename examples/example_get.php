9186bc70081b07d68b76ae9f2e367bb50c6dc053
ANDRES Lopez <anlopezlara@gmail.com>
2018-01-31 08:07:27 -0800

    primer boton

--- examples/example_get.php
+++ examples/example_get.php
@@ -5,7 +5,7 @@ $meli = new Meli($appId, $secretKey);
 $params = array();
 $url = '/sites/' . $siteId;
 
-$result = $meli->get($url, array('nickname' => 'RODIA2000'));
+$result = $meli->get('/', array('nickname' => 'RODIA2000'));
 echo '<pre>';
 print_r('<p>$appId    -->'.$appId    .'</p>');
 print_r('<p>$secretKey-->'.$secretKey.'</p>');
