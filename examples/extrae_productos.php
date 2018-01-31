cba14672bf358df107b0108a2a7191f92900c81a
ANDRES Lopez <anlopezlara@gmail.com>
2018-01-31 14:03:54 -0800

    primer boton

--- examples/extrae_productos.php
+++ examples/extrae_productos.php
@@ -11,13 +11,13 @@ $result = $meli->get($url, $params);
 echo '<pre>';
 print_r('<p>2</p>');
 
-print_r($result[body]);
+print_r($result[body][id]);
 /*foreach ( $result as $key => &$value )
 {
     print_r('<p>'.$key  .'</p>');
 	print_r('<p>'.$value.'</p>');
 }*/
 print_r('<p>********************************************************************</p>');
-print_r($result);
+print_r($result[body]);
 
 echo '</pre>';
