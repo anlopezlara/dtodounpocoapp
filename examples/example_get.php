80227adf741f5ca5372823cf5013f76401885ee3
ANDRES Lopez <anlopezlara@gmail.com>
2018-01-31 07:27:50 -0800

    primer boton

--- examples/example_get.php
+++ examples/example_get.php
@@ -1,18 +1,10 @@
 <?php
 require '../Meli/meli.php';
 require '../configApp.php';
-
 $meli = new Meli($appId, $secretKey);
-
 $params = array();
-
 $url = '/sites/' . $siteId;
-
 $result = $meli->get($url, $params);
-
 echo '<pre>';
-println('params' . $params);
-println('siteId' . $siteId);
-println('url' . $url);
-printr($result);
+print_r($result);
 echo '</pre>';
