d20f7c16d6d97747962204c3e8c052eb2c4997fe
ANDRES Lopez <anlopezlara@gmail.com>
2018-01-31 09:52:02 -0800

    primer boton

--- examples/extrae_productos.php
+++ examples/extrae_productos.php
@@ -9,8 +9,9 @@ $result = $meli->get($url, $params);
 echo '<pre>';
 print_r('<p>3</p>');
 
-print_r($result['body']);
-
+print_r($result['body'][results]);
+print_r('************************************************************');
+print_r('************************************************************');
 #foreach ($result as $i => $value) {
 #    unset($array[$i]);
 #}
