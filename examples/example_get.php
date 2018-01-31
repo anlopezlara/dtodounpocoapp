606548fe6d0c05f88819b7d3459facdfe0a7f8e0
ANDRES Lopez <anlopezlara@gmail.com>
2018-01-31 08:19:20 -0800

    primer boton

--- examples/example_get.php
+++ examples/example_get.php
@@ -2,15 +2,15 @@
 require '../Meli/meli.php';
 require '../configApp.php';
 $meli = new Meli($appId, $secretKey);
-$params = array();
+$params = array("nickname" => "RODIA2000");
 $url = '/sites/' . $siteId;
 
-$result = $meli->get($url, array('nickname' => 'RODIA2000'));
+$result = $meli->get($url, $params);
 echo '<pre>';
-print_r('<p>1$appId    -->'.$appId    .'</p>');
-print_r('<p>1$secretKey-->'.$secretKey.'</p>');
-print_r('<p>1$params   -->'.$params   .'</p>');
-print_r('<p>1$url      -->'.$url      .'</p>');
-print_r('<p>1$siteId   -->'.$siteId   .'</p>');
+print_r('<p>2$appId    -->'.$appId    .'</p>');
+print_r('<p>$secretKey-->'.$secretKey.'</p>');
+print_r('<p>$params   -->'.$params   .'</p>');
+print_r('<p>$url      -->'.$url      .'</p>');
+print_r('<p>$siteId   -->'.$siteId   .'</p>');
 print_r($result);
 echo '</pre>';
