172723987a94e3bacb36160c19f7221cdb1f47c4
ANDRES Lopez <anlopezlara@gmail.com>
2018-01-31 13:47:30 -0800

    primer boton

--- examples/extrae_productos.php
+++ examples/extrae_productos.php
@@ -4,12 +4,12 @@ require '../configApp.php';
 $meli = new Meli($appId, $secretKey);
 #$params = array('Producto'=>'MLM602329981');
 $params = array();
-#$url = '/sites/' . $siteId. '/items/MLM573165050';
-$url = '/items/MLM573165050';
+#$url = '/sites/' . $siteId. '/items/MLM602329981';
+$url = '/items/MLM602329981';
 
 $result = $meli->get($url, $params);
 echo '<pre>';
-print_r('<p>4</p>');
+print_r('<p>1</p>');
 
 print_r($result);
 
