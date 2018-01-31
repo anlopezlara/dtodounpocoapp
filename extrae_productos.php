a97cfdc00a5c7c339e57e367dae4a305b989cb7a
ANDRES Lopez <anlopezlara@gmail.com>
2018-01-31 09:07:34 -0800

    primer boton

---
+++ extrae_productos.php
@@ -0,0 +1,12 @@
+<?php
+require '../Meli/meli.php';
+require '../configApp.php';
+$meli = new Meli($appId, $secretKey);
+$params = array('nickname'=>$nickname);
+$url = '/sites/' . $siteId. '/search';
+
+$result = $meli->get($url, $params);
+echo '<pre>';
+print_r('<p>5</p>');
+print_r($result);
+echo '</pre>';
