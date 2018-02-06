a60c8ec5088ba6eb4c3d47af6dd8231fbbc50f90
ANDRES Lopez <anlopezlara@gmail.com>
2018-02-06 15:47:59 -0800

    primer boton

--- examples/base_datos_2.php
+++ examples/base_datos_2.php
@@ -1,7 +1,9 @@
-<form method="post" action="yourFileName.php">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
     <input type="text" name="studentname">
     <input type="submit" value="click" name="submit"> <!-- assign a name for the button -->
 </form>
+
+
 
 <?php
 function display()
