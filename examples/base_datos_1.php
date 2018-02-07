b2267e09eacface8cc025d748c3b1bcc8df9a695
ANDRES Lopez <anlopezlara@gmail.com>
2018-02-07 10:41:14 -0800

    primer boton

--- examples/base_datos_1.php
+++ examples/base_datos_1.php
@@ -39,6 +39,24 @@ print '</form>';
 function display()
 {
 	echo "Prueba_1";
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Prueba";
+
+	$sql = "SELECT ref, label, price, accountancy_code_sell, stock
+			  FROM i3120427_doli1.doli_product
+			 WHERE ref in ('T0057','T0338')";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<p>ref: " . $row["ref"]. " - label: " . $row["label"]. " " . " - price: " . $row["price"]. " - accountancy_code_sell: " . $row["accountancy_code_sell"]. " - stock: " . $row["stock"]. "</p>";
+		}
+	} else {
+		echo "0 results";
+	}
 }
 if(isset($_POST['submit']))
 {
