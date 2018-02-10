1e002058b0e19301438678dbee04b6f0abab2408
ANDRES Lopez <anlopezlara@gmail.com>
2018-02-09 20:30:13 -0800

    primer boton

--- examples/base_datos_3.php
+++ examples/base_datos_3.php
@@ -1,91 +1,49 @@
 <?php
-session_start();
 require '../Meli/meli.php';
 require '../configApp.php';
-print '<!DOCTYPE html>';
-print '<html>';
-	print '<head>';
-	print '<style>';
-		print 'table {';
-		print '    font-family: arial, sans-serif;';
-		print '    border-collapse: collapse;';
-		print '    width: 100%;';
-		print '}';
+$meli = new Meli($appId, $secretKey);
+#$params = array('Producto'=>'MLM602329981');
+$params = array();
+#$url = '/sites/' . $siteId. '/items/MLM602329981';
+$url = '/items/MLM602329981';
+$servername = "www.d-todounpoco.com.mx";
+$username = "anlopez";
+$password = "Azteca02";
 
-		print 'td, th {';
-		print '    border: 1px solid #dddddd;';
-		print '    text-align: left;';
-		print '    padding: 8px;';
-		print '}';
+print '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">  ';
+print '    <input type="submit" value="click" name="submit"> <!-- assign a name for the button -->';
+print '</form>';
 
-		print 'tr:nth-child(even) {';
-		print '    background-color: #dddddd;';
-		print '}';
-		print '</style>';
-	print '</head>';
-	
-	print '<body>';
-		print '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">  ';
-		print '    <input type="submit" value="click" name="submit"> <!-- assign a name for the button -->';
-		print '</form>';
 
-		function display($servername_,$username_,$password_)
-		{
-			// Variables Mercado Libre
-			require '../Meli/meli.php';
-			require '../configApp.php';
-			$meli = new Meli($appId, $secretKey);
-			$params = array();
-			$url = '/items/MLM602329981';
-			//
-			$conn = new mysqli($servername_, $username_, $password_);
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			} 
-			$sql = "SELECT ref, label, price, accountancy_code_sell, stock
-					  FROM i3120427_doli1.doli_product
-					 WHERE accountancy_code_sell != ''
-					   AND ref in ('T0057','T0338')";
-					 // AND substring(ref,1,1) = 'T'";
-					 //AND ref in ('T0057','T0338')";
-			$result = $conn->query($sql);
+function display()
+{
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
 
-			if ($result->num_rows > 0) {
-				// output data of each row
-				print '<table class="border" width="100%">';
-				print "<td>ID Dolibar</td><td>ID MercLibre </td><td>Descripción</td><td>Precio Dolibar</td><td>Stock Dolibar</td>";
-				while($row = $result->fetch_assoc()) {
-					//$resultml = $meli->get($url, $params);
-					//foreach ( $result[body] as $key => &$value )
-					//{
-					//	if ($key == 'id') {
-					//		print_r('<p>'.$key.' -->'.$value.'</p>');
-					//	}
-					//	if ($key == 'title') {
-					//		print_r('<p>'.$key.' -->'.$value.'</p>');
-					//	}
-					//	if ($key == 'price') {
-					//		print_r('<p>'.$key.' -->'.$value.'</p>');
-					//	}
-					//	if ($key == 'available_quantity') {
-					//		print_r('<p>'.$key.' -->'.$value.'</p>');
-					//	}
-					//	if ($key == 'status') {
-					//		print_r('<p>'.$key.' -->'.$value.'</p>');
-					//	}
-					//}
-					print "<tr>";
-					print "<td>".$row["ref"]. "</td><td>" . $row["accountancy_code_sell"]." </td><td>".$row["label"]." </td><td>" . $row["price"]."</td><td>" . $row["stock"]."</td>";
-					print "</tr>";
-				}
-				print '</table>';
-			} else {
-				print "0 results";
-			}
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Prueba";
+
+	$sql = "SELECT ref, label, price, accountancy_code_sell, stock
+			  FROM i3120427_doli1.doli_product
+			 WHERE ref in ('T0057','T0338')";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<p>ref: " . $row["ref"]. " - label: " . $row["label"]. " " . " - price: " . $row["price"]. " - accountancy_code_sell: " . $row["accountancy_code_sell"]. " - stock: " . $row["stock"]. "</p>";
 		}
-		if(isset($_POST['submit']))
-		{
-		   display($servername,$username,$password);
-		} 
-	print '</body>';
-print '</html>';
+	} else {
+		echo "0 results";
+	}
+
+	$conn->close();
+	
+}
+if(isset($_POST['submit']))
+{
+   display();
+} 
