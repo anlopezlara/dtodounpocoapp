1330feca2cf9c58191443a11b730d5ec88f508cb
ANDRES Lopez <anlopezlara@gmail.com>
2018-02-13 20:46:23 -0800

    primer boton

--- index.php
+++ index.php
@@ -13,26 +13,32 @@ if(isset($_GET['code']) || isset($_SESSION['access_token'])) {
 	
 	if(isset($_GET['code']) && !isset($_SESSION['access_token'])) {
 		// //If the code was in get parameter we authorize
+		print_r('<p>PASA 1</p>');
 		try{
+			print_r('<p>PASA 2</p>');
 			$user = $meli->authorize($_GET["code"], $redirectURI);
-			
+			print_r('<p>PASA 3</p>');
 			// Now we create the sessions with the authenticated user
 			$_SESSION['access_token'] = $user['body']->access_token;
 			$_SESSION['expires_in'] = time() + $user['body']->expires_in;
 			$_SESSION['refresh_token'] = $user['body']->refresh_token;
+			print_r('<p>PASA 4</p>');
 		}catch(Exception $e){
 			echo "Exception: ",  $e->getMessage(), "\n";
 		}
 	} else {
 		// We can check if the access token in invalid checking the time
 		if($_SESSION['expires_in'] < time()) {
+			print_r('<p>PASA 5</p>');
 			try {
+				print_r('<p>PASA 6</p>');
 				// Make the refresh proccess
 				$refresh = $meli->refreshAccessToken();
 				// Now we create the sessions with the new parameters
 				$_SESSION['access_token'] = $refresh['body']->access_token;
 				$_SESSION['expires_in'] = time() + $refresh['body']->expires_in;
 				$_SESSION['refresh_token'] = $refresh['body']->refresh_token;
+				print_r('<p>PASA 7</p>');
 			} catch (Exception $e) {
 			  	echo "Exception: ",  $e->getMessage(), "\n";
 			}
