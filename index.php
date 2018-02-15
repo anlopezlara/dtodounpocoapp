516da0e809de9e0af8636406d223e17b1784e709
ANDRES Lopez <anlopezlara@gmail.com>
2018-02-15 11:00:46 -0800

    primer boton

--- index.php
+++ index.php
@@ -13,25 +13,14 @@ $appName = explode('.', $domain)[0];
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <meta name="description" content="Official PHP SDK for Mercado Libre's API.">
-        <meta name="keywords" content="API, PHP, Mercado Libre, SDK, meli, integration, e-commerce">
-        <title>Mercado Libre PHP SDK</title>
+        <meta name="description" content="De todo un poco portal">
+        <meta name="keywords" content="De todo un poco portal">
+        <title>De todo un poco portal</title>
         <link rel="stylesheet" href="/getting-started/style.css" />
         <script src="script.js"></script>
     </head>
 
     <body>
-        <header class="navbar">
-            <a class="logo" href="#"><img src="/getting-started/logo-developers.png" alt=""></a>
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a target="_blank" href="http://developers.mercadolibre.com/getting-started/">Getting Started</a></li>
-                    <li><a target="_blank" href="http://developers.mercadolibre.com/api-docs/">API Docs</a></li>
-                    <li><a target="_blank" href="http://developers.mercadolibre.com/community/">Community</a></li>
-                </ul>
-            </nav>
-        </header>
-
         <div class="header">
             <div>
                 <h1>Getting Started with Mercado Libre's PHP SDK</h1>
