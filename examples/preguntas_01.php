45a513bb4cd7b84d8c66cc5af650aeac0ed9fae4
ANDRES Lopez <anlopezlara@gmail.com>
2018-02-22 09:04:54 -0800

    primer boton

--- examples/preguntas_01.php
+++ examples/preguntas_01.php
@@ -14,11 +14,18 @@ $sql = "SELECT * FROM db_dtodounpoco.sesion_meli";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-	print_r($result);	
+	print_r('<p>'.$result.'</p>');	
 	while($row = $result->fetch_assoc()) {
-		print_r($row[id_valor]);
-		print_r($row[valor]);
+		if ($row['id_valor'] == 'access_token')
+			$access_token = $row['valor'];
+		if ($row['id_valor'] == 'expires_in')
+			$expires_in = $row['valor'];
+		if ($row['id_valor'] == 'refresh_token')
+			$refresh_token = $row['valor'];
 	}
+	print_r('<p>'.$access_token.'</p>');
+	print_r('<p>'.$expires_in.'</p>');
+	print_r('<p>'.$refresh_token.'</p>');
 } 
 else {
 	print_r("No hubo conexión a la base de datos");
