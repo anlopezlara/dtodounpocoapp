9c46a3f308399d9e0aba2148726551db7c28291c
ANDRES Lopez <anlopezlara@gmail.com>
2018-02-22 09:12:19 -0800

    primer boton

--- examples/preguntas_01.php
+++ examples/preguntas_01.php
@@ -12,23 +12,26 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM db_dtodounpoco.sesion_meli";
 
 $result = $conn->query($sql);
-echo '<pre>';
-if ($result->num_rows > 0) {
-	print('<p>'.$result.'</p>');	
-	/*while($row = $result->fetch_assoc()) {
-		if ($row['id_valor'] == 'access_token')
-			$access_token = $row['valor'];
-		if ($row['id_valor'] == 'expires_in')
-			$expires_in = $row['valor'];
-		if ($row['id_valor'] == 'refresh_token')
-			$refresh_token = $row['valor'];
-	}
-	print_r('<p>'.$access_token.'</p>');
-	print_r('<p>'.$expires_in.'</p>');
-	print_r('<p>'.$refresh_token.'</p>');*/
-echo '</pre>';
-} 
-else {
-	print_r("No hubo conexión a la base de datos");
-			
-}
+print '<html>';
+	print '<body>';
+		if ($result->num_rows > 0) {
+			print('<p>'.$result.'</p>');	
+			/*while($row = $result->fetch_assoc()) {
+				if ($row['id_valor'] == 'access_token')
+					$access_token = $row['valor'];
+				if ($row['id_valor'] == 'expires_in')
+					$expires_in = $row['valor'];
+				if ($row['id_valor'] == 'refresh_token')
+					$refresh_token = $row['valor'];
+			}
+			print_r('<p>'.$access_token.'</p>');
+			print_r('<p>'.$expires_in.'</p>');
+			print_r('<p>'.$refresh_token.'</p>');*/
+
+		} 
+		else {
+			print_r("No hubo conexión a la base de datos");
+					
+		}
+	print '</body>';
+print '</html>';
