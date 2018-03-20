bafa8a7de49c21db22c8fad2b6645e154da749be
ANDRES Lopez <anlopezlara@gmail.com>
2018-03-20 16:41:18 -0700

    primer boton

--- examples/inserta_pedido_tlapalero.php
+++ examples/inserta_pedido_tlapalero.php
@@ -2,10 +2,8 @@
 session_start();
 require 'Meli/meli.php';
 require 'configApp.php';
-$domain = $_SERVER['HTTP_HOST'];
-$appName = explode('.', $domain)[0];
-
-
+#$domain = $_SERVER['HTTP_HOST'];
+#$appName = explode('.', $domain)[0];
 
 $meli = new Meli($appId, $secretKey);
 date_default_timezone_set('America/Mexico_City');
@@ -20,10 +18,11 @@ if ($conn->connect_error) {
 
 $sql = "SELECT * FROM db_dtodounpoco.sesion_meli";
 $result = $conn->query($sql);
-
-print '<div class="row">';
-	print '<div class="col-sm-1 col-md-1">';
-		print '<h3>Inserta Pedidos Tlapalero</h3>';
-        print '<p><a class="btn" href="../examples/inserta_pedido_tlapalero.php">Pedidos Tlapalero</a></p>';
-    print '</div>';
-print '</div>';
+print '<main class="container">';
+	print '<div class="row">';
+		print '<div class="col-sm-1 col-md-1">';
+			print '<h3>Inserta Pedidos Tlapalero</h3>';
+			print '<p><a class="btn" href="../examples/inserta_pedido_tlapalero.php">Pedidos Tlapalero</a></p>';
+		print '</div>';
+	print '</div>';
+print '</main>';
