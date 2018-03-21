7b335e2603b77b87f8c7bc7277dc9cfc91b42f6c
ANDRES Lopez <anlopezlara@gmail.com>
2018-03-21 15:18:53 -0700

    primer boton

--- examples/inserta_pedido_tlapalero.php
+++ examples/inserta_pedido_tlapalero.php
@@ -9,50 +9,38 @@ $now  = new DateTime;
 $duration=60;
 $dateinsec=strtotime($now->format('Y-m-d H:i:s'));
 
-$conn = new mysqli($servername, $username, $password);
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-} 
+print '<!DOCTYPE html>';
+print '<html>';
+	print '<head>';
+	print '<style>';
+		print 'table {';
+		print '    font-family: arial, sans-serif;';
+		print '    border-collapse: collapse;';
+		print '    width: 100%;';
+		print '}';
 
-$sql = 'SELECT pedido, date_format(fecha,"%d/%m/%Y") fecha_pedido FROM db_dtodounpoco.carga_pedido_tlapalero GROUP BY pedido, fecha ORDER BY pedido desc';
-$result = $conn->query($sql);
+		print 'td, th {';
+		print '    border: 1px solid #dddddd;';
+		print '    text-align: left;';
+		print '    padding: 8px;';
+		print '}';
 
-// define variables and set to empty values
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $gender = test_input($_POST["gender"]);
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-
-Print '<h2>Pedido Tlapalero</h2>';
-echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">';
-Print '  <input type="radio" name="gender" value="female">Female <br>';
-Print '  <input type="radio" name="gender" value="male">Male <br>';
-
-if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		#echo $row["pedido"].' - '.$row["fecha_pedido"].'<br>';
-		echo '<input type="radio" name="gender" value="'.$row["pedido"].'">'.$row["pedido"].' - '.$row["fecha_pedido"].'<br>';
-	}
-}
-
-Print '<br><br>';
-Print '  <input type="submit" name="submit" value="Submit">';
-Print '</form>';
-
-echo "<h2>Your Input:</h2>";
-echo "<br>";
-echo $gender;
-
-if ($result->num_rows > 0) {
-	while($row = $result->fetch_assoc()) {
-		echo $row["pedido"].' - '.$row["fecha_pedido"].'<br>';
-	}
-}
+		print 'tr:nth-child(even) {';
+		print '    background-color: #dddddd;';
+		print '}';
+		print '</style>';
+	print '</head>';
+	print '<body>';
+		print '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">  ';
+			print '<input type="submit" value="click" name="submit"> <!-- assign a name for the button -->';
+		print '</form>';
+		function display($servername_,$username_,$password_,$meli_,$params_,$url_)
+		{
+			echo 'boton';
+		}
+		if(isset($_POST['submit']))
+		{
+		   display($servername,$username,$password,$meli,$params,$url);
+		} 
+	print '</body>';
+print '</html>';
