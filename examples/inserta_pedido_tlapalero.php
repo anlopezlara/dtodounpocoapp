f4a7a9bc5f02c081f8386605559c33329c2f343d
ANDRES Lopez <anlopezlara@gmail.com>
2018-03-21 15:28:10 -0700

    primer boton

--- examples/inserta_pedido_tlapalero.php
+++ examples/inserta_pedido_tlapalero.php
@@ -9,6 +9,17 @@ $now  = new DateTime;
 $duration=60;
 $dateinsec=strtotime($now->format('Y-m-d H:i:s'));
 
+$conn = new mysqli($servername_, $username_, $password_);
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = 'SELECT pedido, date_format(fecha,"%d/%m/%Y") fecha_pedido 
+		  FROM db_dtodounpoco.carga_pedido_tlapalero 
+		 GROUP BY pedido, fecha 
+		 ORDER BY pedido desc';
+$result = $conn->query($sql);
+
 print '<!DOCTYPE html>';
 print '<html>';
 	print '<head>';
@@ -32,31 +43,17 @@ print '<html>';
 	print '</head>';
 	print '<body>';
 		print '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">  ';
-			
-			print '<input type="submit" value="click" name="submit"> <!-- assign a name for the button -->';
-		print '</form>';
-		function display($servername_,$username_,$password_)
-		{
-			$conn = new mysqli($servername_, $username_, $password_);
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			} 
-			
-			$sql = 'SELECT pedido, date_format(fecha,"%d/%m/%Y") fecha_pedido 
-			          FROM db_dtodounpoco.carga_pedido_tlapalero 
-					 GROUP BY pedido, fecha 
-					 ORDER BY pedido desc';
-			$result = $conn->query($sql);
-			
-			
 			if ($result->num_rows > 0) {
 				while($row = $result->fetch_assoc()) {
 					#echo $row["pedido"].' - '.$row["fecha_pedido"].'<br>';
 					echo '<input type="radio" name="gender" value="'.$row["pedido"].'">$row["pedido"].' - '.$row["fecha_pedido"]<br>';
 				}
 			}
-			
-			
+			print '<input type="submit" value="click" name="submit"> <!-- assign a name for the button -->';
+		print '</form>';
+		function display($servername_,$username_,$password_)
+		{
+			echo 'Prueba 1';
 		}
 		if(isset($_POST['submit']))
 		{
