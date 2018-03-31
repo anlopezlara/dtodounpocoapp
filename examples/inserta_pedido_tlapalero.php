eb2572b7847d9cc0077b1c7f0f113f53adfc5f18
ANDRES Lopez <anlopezlara@gmail.com>
2018-03-31 10:46:24 -0700

    primer boton

--- examples/inserta_pedido_tlapalero.php
+++ examples/inserta_pedido_tlapalero.php
@@ -162,7 +162,7 @@ print '<html>';
 				
 				#------------------------------------------------------------------------------------------------
 				#------------------------------------------------------------------------------------------------
-				$mysqli = new mysqli($servername, $db_username, $password_, "db_dtodounpoco");
+				$mysqli = new mysqli("www.d-todounpoco.com.mx", "anlopez_desa", "Azteca02", "db_dtodounpoco");
 				if ($mysqli->connect_errno) {
 					echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 				}
