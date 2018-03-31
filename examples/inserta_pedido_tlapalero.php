5c5d77670b93e533c63eb4bd07528e911d86042e
ANDRES Lopez <anlopezlara@gmail.com>
2018-03-31 11:07:39 -0700

    primer boton

--- examples/inserta_pedido_tlapalero.php
+++ examples/inserta_pedido_tlapalero.php
@@ -167,12 +167,6 @@ print '<html>';
 					echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 				}
 
-				if (!$mysqli->query("DROP PROCEDURE IF EXISTS p") ||
-					!$mysqli->query('CREATE PROCEDURE p(OUT msg VARCHAR(50)) BEGIN SELECT "Hi!" INTO msg; END;')) {
-					echo "Stored procedure creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
-				}
-
-
 				if (!$mysqli->query("SET @msg = ''") || !$mysqli->query("CALL p(@msg)")) {
 					echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
 				}
